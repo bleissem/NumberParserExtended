0f00e100a391f28735a53c8475617776713ebb4f
Alexander Bleissem <alexander@bleissem.com>
2018-03-19 00:08:30 +0100

    build content

---
+++ NumberParserExtended.Common/BuildContentField.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace NumberParserExtended.Common
+{
+    public class BuildContentField
+    {
+
+        public ContentField Build(string content)
+        {
+            ContentField result = new ContentField();
+        
+            // y -> x -> ContentFxield
+            SortedDictionary<int, SortedDictionary<int, ContentField>> mappedField = new SortedDictionary<int, SortedDictionary<int, ContentField>>();
+
+            string[] contents = content.Split(Environment.NewLine, StringSplitOptions.None);
+            char currentCh = ' ';
+
+            for (int y = 0; y < contents.Length; ++y)
+            {
+                char[] oneCharLine = contents[y].Replace("\t", "   ").ToCharArray();
+
+                for (int x = 0; x < oneCharLine.Length; ++x)
+                {
+                    currentCh = oneCharLine[x];
+                    if (!currentCh.Equals(ContentFieldConstants.EmptyContent))
+                    {                      
+                        if (!mappedField.ContainsKey(y))
+                        {
+                            mappedField.Add(y, new SortedDictionary<int, ContentField>());
+                        }
+
+                        if (!mappedField[y].ContainsKey(x))
+                        {
+                            mappedField[y].Add(x, new ContentField(currentCh));
+                        }
+                        else
+                        {
+                            Trace.Fail("fail");
+                        }
+                    }
+                }
+            }
+
+
+            /// TopLeft     Top         TopRight 
+            /// Left        Content     Right
+            /// BottomLeft  Bottom      BottomRight
+
+            int yCount = mappedField.Keys.Count;
+            int xCount = 0;
+            int xLeft = 0;
+            int xRight = 0;
+            int yUp = 0;
+            int yBottom = 0;
+
+            ContentField currentField = null;
+            ContentField tmpField = null;
+
+            for (int y = 0; y < yCount; ++y)
+            {
+                yUp = (y == 0 ? yCount - 1 : y - 1);
+                yBottom = (y + 1) % yCount;
+
+                xCount = mappedField[y].Count;
+                for (int x = 0; x < xCount; ++x)
+                {
+                    xLeft = (x == 0 ? xCount - 1 : x - 1);
+                    xRight = (x + 1) % xCount;
+
+                    currentField = mappedField[y][x];
+
+                    // topleft 
+                    if (mappedField[yUp].TryGetValue(xLeft, out tmpField))
+                    {
+                        currentField.TopLeft = tmpField;
+                    }
+
+                    // top 
+                    if (mappedField[yUp].TryGetValue(x, out tmpField))
+                    {
+                        currentField.Top = tmpField;
+                    }
+
+                    // topright
+                    if (mappedField[yUp].TryGetValue(xRight, out tmpField))
+                    {
+                        currentField.TopRight = tmpField;
+                    }
+
+                    // left 
+                    if (mappedField[y].TryGetValue(xLeft, out tmpField))
+                    {
+                        currentField.Left = tmpField;
+                    }
+
+                    // right
+                    if (mappedField[y].TryGetValue(xRight, out tmpField))
+                    {
+                        currentField.Right = tmpField;
+                    }
+
+                    // bottomleft 
+                    if (mappedField[yBottom].TryGetValue(xLeft, out tmpField))
+                    {
+                        currentField.BottomLeft = tmpField;
+                    }
+
+                    // bottom 
+                    if (mappedField[yBottom].TryGetValue(x, out tmpField))
+                    {
+                        currentField.Bottom = tmpField;
+                    }
+
+                    // bottomright
+                    if (mappedField[yBottom].TryGetValue(xRight, out tmpField))
+                    {
+                        currentField.BottomRight = tmpField;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- NumberParserExtended/Program.cs
+++ NumberParserExtended/Program.cs
@@ -1,13 +1,42 @@
-﻿using System;
+﻿using NumberParserExtended.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
 
 namespace NumberParserExtended
 {
     class Program
-    {
-        static void Main(string[] args)
+    { 
+
+        static async Task Main(string[] args)
         {
-            
-            Console.WriteLine("Hello World!");
+            try
+            {
+                var assembly = Assembly.GetExecutingAssembly();
+                var resourceName = "NumberParserExtended.NumberParserExtended.txt";
+
+                string contentFile = string.Empty;
+
+                using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    contentFile = await reader.ReadToEndAsync();
+                }
+
+                BuildContentField buildContentField = new BuildContentField();
+                buildContentField.Build(contentFile);
+
+                Console.WriteLine("Hello World!");
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
