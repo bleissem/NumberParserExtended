460769fb819d4cd23d6a468285c0cf25a3a4b035
Alexander Bleissem <alexander@bleissem.com>
2018-03-18 20:36:13 +0100

    initial commit

---
+++ NumberParserExtended.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NumberParserExtended.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ NumberParserExtended/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NumberParserExtended
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
